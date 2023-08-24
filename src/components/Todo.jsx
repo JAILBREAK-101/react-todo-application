18849658ef54bd843690287534447025781a4340
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-08-25 00:49:40 +0200

    add delete todo function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import EditTodoModal from "../hooks/useModal";
+import useModal from "../hooks/useModal";
 
 export default function Todo({ todo }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const handleTodoStatus = () => (todo.completed = !todo.completed);
+  const todoItemRef = useRef();
+
+  function handleEditModal() {
+    setIsModalOpen(() => !isModalOpen);
+    alert(todoItemRef.current.innerText);
+  }
+
+  function deleteTodo() {
+    todo.remove()
+  }
 
   return (
-    <ul>
-      {JSON.parse(localStorage.getItem("todoItems")).map((eachTodo) => {
-        return (
-          <li key={eachTodo.id} className="todo-item">
-            <input
-              id={eachTodo.id}
-              type="checkbox"
-              onInput={handleTodoStatus}
-            />
-            <label htmlFor={eachTodo.id}>{eachTodo.item}</label>
+    <ul className="todo-list">
+      {todo.length == 0 ? (
+        <p className="todo-indicator">No Todo Item Added</p>
+      ) : (
+        todo.map((eachTodo) => {
+          return (
+            <li key={eachTodo.id} className="todo-item">
+              <input
+                id={eachTodo.id}
+                type="checkbox"
+                onInput={handleTodoStatus}
+              />
+              <label ref={todoItemRef} htmlFor={eachTodo.id}>
+                {eachTodo.item}
+              </label>
 
-            <div className="todo-btn">
-              <button className="todo-btn__edit">Edit Item</button>
-              <button className="todo-btn__delete">Delete Item</button>
-            </div>
-          </li>
-        );
-      })}
+              <div className="todo-btn">
+                <button onClick={handleEditModal} className="todo-btn__edit">
+                  Edit Item
+                </button>
+                <button onClick={deleteTodo} className="todo-btn__delete">
+                  Delete Item
+                </button>
+              </div>
+              {isModalOpen ? (
+                <EditTodoModal todoValue={eachTodo.item} />
+              ) : (
+                <></>
+              )}
+            </li>
+          );
+        })
+      )}
     </ul>
   );
 }
+
+// TODO: MAKE EDIT TODO MODAL RENEWABLE AS A CUSTOM HOOK AND CODE DELETE TODO MODAL
